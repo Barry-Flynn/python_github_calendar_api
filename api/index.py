b45e1d2e0d2c7a9afe558ae482027074488f725e
百里飞洋 <2856090148@qq.com>
2023-01-12 14:17:48 +0800

    2023-01-12 Github 页面改版，更新 regex 规则
    参考如下链接，更新了正则使其可以继续运行
    https://github.com/Zfour/python_github_calendar_api/pull/5

--- api/index.py
+++ api/index.py
@@ -10,10 +10,13 @@ def getdata(name):
     gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
     datadatereg = re.compile(r'data-date="(.*?)" data-level')
-    datacountreg = re.compile(r'data-count="(.*?)" data-date')
+    # 注释掉原有正则，更新新的正则使其可以继续使用 https://github.com/Zfour/python_github_calendar_api/pull/5
+    # datacountreg = re.compile(r'data-count="(.*?)" data-date')
+    datacountreg = re.compile(r'rx="2" ry="2">(.*?) contribution')
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
-    datacount = list(map(int, datacount))
+    # datacount = list(map(int, datacount))
+    datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
     contributions = sum(datacount)
     datalist = []
     for index, item in enumerate(datadate):
